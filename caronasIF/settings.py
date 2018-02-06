3c2d86fbf380c41af0ce5664d1a51fa1acdf20e4
Fernando Zanutto <ferzanutto1999@gmail.com>
2018-02-06 20:36:02 -0200

    Added django-widget-tweaks
    Added django-widget-tweaks
    Added .gitignore

--- caronasIF/settings.py
+++ caronasIF/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'caronas'
+    'caronas',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
